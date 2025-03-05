b373938505d1ebe8083739822500e599f0816350
xavierblakeforbes <xavierblakeforbes@gmail.com>
2025-03-05 17:22:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let quotes = [ "Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love can do that", " Even if you feel that only darkness lies ahead... As long as you hold hands together... See it through as one... ...the world will never end! The world exists within all of you.", "You have to believe in yourself when no one else does", "Keep moving forward"];
+let quote = quotes[0]; // the first quote 
+function logfirstquote()
+{
+  let randomNumber= Math.floor(Math.random()*quotes.length);
+  console.log(quotes[randomNumber]);
+}
+logfirstquote();
